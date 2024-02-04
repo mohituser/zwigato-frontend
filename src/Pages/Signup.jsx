422642ab5f5a173e3e651d975cd76c592272d60d
mohituser <122739741+mohituser@users.noreply.github.com>
2024-02-04 14:40:42 +0530

    Update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -40,7 +40,7 @@ function Signup() {
         }
         try {
             
-            const response=await  axios.post("http://localhost:5002/register",{...loginData},{withCredentials:true});
+            const response=await  axios.post("https://zwigato-backend-dm7f.onrender.com/register",{...loginData},{withCredentials:true});
             // console.log(response);
             if(response?.data?.success){
                 console.log(response);
